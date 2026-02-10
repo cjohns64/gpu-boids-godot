443fc6154aa2e57fd37b8d676e1f96683f24fc6a
Cory Johns <cory@triplevisiongroup.com>
2026-02-10 08:39:41 -0800

    Worked on variable swim rate -- buggy --

--- Fish/spine_fish_ctrl.gd
+++ Fish/spine_fish_ctrl.gd
@@ -1,19 +1,26 @@
 extends Node3D
 
 @onready var retopo: MeshInstance3D = $retopo
+var swim_rate:float = 2.0
+var last:float = 2.0
+var tween:Tween
+var t:float = 0.0
 
-func set_swim_rate(rate:float) -> void:
-	var r:float = rate
+func __set_swim_rate__(rate:float) -> void:
+	if rate > 20.0:
+		swim_rate = 20.0
 	if rate < 2.0:
-		r = 2.0
-	elif rate > 20.0:
-		r = 20.0
-	print(retopo.mesh.surface_get_material(0).get_shader_parameter("rate"))
-	retopo.mesh.surface_get_material(0).set_shader_parameter("rate", r)
-	print(retopo.mesh.surface_get_material(0).get_shader_parameter("rate"))
+		swim_rate = 2.0
+	if rate >= 2.0 and rate <= 20.0:
+		retopo.mesh.surface_get_material(0).set_shader_parameter("rate", rate)
+		swim_rate = rate
+	else:
+		retopo.mesh.surface_get_material(0).set_shader_parameter("rate", swim_rate)
 
 func _input(event):
 	if event.is_action_pressed("Swim Faster"):
-		set_swim_rate(15.0)
+		tween = get_tree().create_tween().set_parallel(true)
+		tween.tween_method(__set_swim_rate__, swim_rate, swim_rate + 2.0, 0.5)
 	elif event.is_action_pressed("Swim Slower"):
-		set_swim_rate(2.0)
+		tween = get_tree().create_tween().set_parallel(true)
+		tween.tween_method(__set_swim_rate__, swim_rate, swim_rate - 2.0, 0.5)
